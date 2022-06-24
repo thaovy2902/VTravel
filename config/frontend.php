ba8a67d7ce85272456ef37b6946fe7d51daa7db7
thaovy2902 <thaovyduong2902@gmail.com>
2022-06-24 14:07:44 +0700

    Done fix reset password

--- config/frontend.php
+++ config/frontend.php
@@ -3,5 +3,5 @@
 return [
   'frontend_url' => env('MIX_APP_URL', 'https://vtravel-dut.herokuapp.com'),
 
-  'response_password_reset_url' => env('RESPONSE_PASSWORD_RESET_URL', '/response-password-reset?token='),
+  'response_password_reset_url' => env('RESPONSE_PASSWORD_RESET_URL', 'response-password-reset?token='),
 ];
